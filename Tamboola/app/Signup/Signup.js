69e4049ff2216ea0842e3e2e7da4bbcea5e82a15
yajatvishwak <yajat472001@gmail.com>
2020-04-27 17:37:05 +0530

    V1 - with login

--- Tamboola/app/Signup/Signup.js
+++ Tamboola/app/Signup/Signup.js
@@ -10,9 +10,31 @@ import {
 
 function Signup({ props, navigation }) {
   const [nameText, setnameText] = useState("");
+  const [passText, setpassText] = useState("");
 
   const handlePress = () => {
-    navigation.replace("Main", { id: nameText });
+    fetch("http://192.168.43.1:3000/auth", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userID: nameText,
+        password: passText,
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+
+        if (json == true) {
+          navigation.replace("Main", { id: nameText });
+        } else {
+          alert("Invalid Credentials");
+        }
+      })
+      .catch((err) => alert("Some Error " + err));
   };
 
   return (
@@ -20,12 +42,18 @@ function Signup({ props, navigation }) {
       <StatusBar hidden={true}></StatusBar>
 
       <TextInput
-        placeholder="Name"
+        placeholder="user id"
         style={styles.name}
         onChangeText={(text) => setnameText(text)}
       ></TextInput>
+      <TextInput
+        placeholder="password"
+        secureTextEntry={true}
+        style={styles.pass}
+        onChangeText={(text) => setpassText(text)}
+      ></TextInput>
       <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.play}>Play!</Text>
+        <Text style={styles.play}>play!</Text>
       </TouchableOpacity>
     </View>
   );
@@ -40,7 +68,7 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0,0,0,1)",
   },
   name: {
-    width: 211,
+    width: 300,
     height: 46,
     color: "#fff",
     borderRadius: 10,
@@ -50,13 +78,24 @@ const styles = StyleSheet.create({
     marginTop: 347,
     alignSelf: "center",
   },
+  pass: {
+    width: 300,
+    height: 46,
+    color: "#fff",
+    borderRadius: 10,
+    borderColor: "rgba(255,255,255,1)",
+    borderWidth: 2,
+    textAlign: "center",
+    marginTop: 25,
+    alignSelf: "center",
+  },
   button: {
-    width: 116,
+    width: 150,
     height: 40,
     backgroundColor: "rgba(255,255,255,1)",
     borderRadius: 10,
-    marginTop: 121,
-    marginLeft: 150,
+    marginTop: 100,
+    alignSelf: "center",
   },
   play: {
     color: "#121212",
